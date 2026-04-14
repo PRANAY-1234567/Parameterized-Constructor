806feb4e21505d67165d0b2aab3a556f57e6c329
Pranay <pranayvjadhao197@gmail.com>
2026-04-14 12:28:26 +0530

    Initial commit

---
+++ Person.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self,name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print(f"Name : {self.name}\tAge : {self.age}")
+
+p1 = Person("Sakshi", 20)
+p2 = Person("Pranay", 12)
+p3 = Person("Jay", 23)
+
+p1.display()
+p2.display()
+p3.display()
